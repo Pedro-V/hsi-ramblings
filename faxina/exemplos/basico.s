f888b49272ddaf39a4cf9742e80f2129c4e0c5a9
Pedro-V <mingas62@hotmail.com>
2023-09-22 10:04:51 -0300

    exemplo basico em assembly

--- faxina/exemplos/basico.s
+++ faxina/exemplos/basico.s
@@ -1,42 +1,32 @@
 # esse código é alimentado ao Defuse para obter codigo de maquina
 # que será o exemplo dado ao programa
+.intel_syntax noprefix
+
 .section .text
     .globl main
-
 main:
-    push %rbp
-    movq %rsp, %rbp
-    movq $1, %rdi        # Initialize rdi with 1 (starting number)
+    push   rbp
+    mov    rbp,rsp
+    mov    r10,0x1
 
-loop_start:
-    # Print the value in rdi
-    movq %rdi, %rax      # Copy rdi to rax
-    addq $'0', %rax      # Convert to ASCII
-    pushq %rax           # Push the ASCII character onto the stack
-    movq $1, %rax        # syscall number for write
-    movl $1, %edi        # write to stdout (int fd=1)
-    movq %rsp, %rsi      # use char on stack
-    movq $1, %rdx        # size_t len = 1 char to write.
-    syscall              # call the kernel, it looks at registers to decide what to do
-
-    # Print a newline character
-    pushq $'\n'          # push qword 0xA (ASCII '\n')
-    movq $1, %rax        # syscall number for write
-    movl $1, %edi        # write to stdout (int fd=1)
-    movq %rsp, %rsi      # use char on stack
-    movq $1, %rdx        # size_t len = 1 char to write.
-    syscall              # call the kernel, it looks at registers to decide what to do
-
-    # Increment rdi (the value to print)
-    incq %rdi
-
-    # Check if we've reached 101 (after printing 100)
-    cmpq $101, %rdi
-    je loop_end
-
-    # Continue the loop
-    jmp loop_start
-
-loop_end:
+loop:
+    mov    rax,r10
+    add    al,0x30
+    push   rax
+    mov    rax,0x1
+    mov    edi,0x1
+    mov    rsi,rsp
+    mov    rdx,0x1
+    syscall
+    push   0xa
+    mov    rax,0x1
+    mov    edi,0x1
+    mov    rsi,rsp
+    mov    rdx,0x1
+    syscall
+    inc    r10
+    cmp    r10,0xa
+    jl     loop
+end:
     leave
-    ret
+    ret 
